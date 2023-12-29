b246eedde0a0dfc97ed60e7df5d8d1a22e174a35
ANKIT6244 <ankitkumarshah5050@gmail.com>
2023-12-29 08:01:11 +0530

    Rmoved unnecessary for loop and switch case.

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -9,26 +9,14 @@ public class simpleCalculator {
         int a = scan.nextInt();
         System.out.print("b = ");
         int b = scan.nextInt();
-
-        for (int i = 1; i <= 4; i++) {
-            switch (i) {
-                case 1:
-                    System.out.println("Add :" + (a + b));
-                    break;
-                case 2:
-                    System.out.println("Sub :" + (a - b));
-                    break;
-                case 3:
-                    System.out.println("Mul :" + (a * b));
-                    break;
-                case 4:
-                    if (b == 0) {
-                        System.out.println("Div : Divide by 0 is not allowed.");
-                    } else
-                        System.out.println("Div :" + (a / b));
-                    break;
-            }
-
+        System.out.println("Add :" + (a + b));
+        System.out.println("Sub :" + (a - b));
+        System.out.println("Mul :" + (a * b));
+        if (b == 0) {
+            System.out.println("Div : Divide by 0 is not allowed.");
+        } else {
+            System.out.println("Div :" + (a / b));
         }
+        scan.close();
     }
 }
